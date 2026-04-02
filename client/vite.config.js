003c66278ad67e4b62b7cf87e42a91d881a42478
Bill Murphy <wfmurphy@gmail.com>
2026-04-02 11:07:48 -0400

    added localhost https to client, as well

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import createHttpsLocalhost from 'https-localhost';
+
+const certs = await createHttpsLocalhost().getCerts();
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: certs,
     proxy: {
       '/api': {
         target: 'https://localhost:3001',
